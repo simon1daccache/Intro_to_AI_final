59f99e354d9f941592d6d6258173766724b03812
simon1daccache <simon1daccache@gmail.com>
2021-06-19 17:29:11 +0300

    created project files

---
+++ bots/__init__.py
@@ -0,0 +1,11 @@
+from .human import Human
+from .minimax import MiniMaxBot
+from .expectimax import ExpectiMaxBot
+from .montecarlo import MonteCarloBot
+
+__all__ = [
+    'Human',
+    'MiniMaxBot',
+    'ExpectiMaxBot',
+    'MonteCarloBot'
+]
